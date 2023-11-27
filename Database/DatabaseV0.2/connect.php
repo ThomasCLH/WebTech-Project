27c952e12bc731eb4def7067982962a001df896a
ThomasCLH <127458003+ThomasCLH@users.noreply.github.com>
2023-11-28 00:49:44 +0800

    Add files via upload

---
+++ Database/DatabaseV0.2/connect.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <title>Insert Data</title>
+</head>
+<body>
+<?php
+
+function addcart($product, $quantity)
+{
+    global $cart, $check;
+
+    if ($product != "" && $quantity > 0 && $check)
+    {
+        echo "$product: $quantity<br/>";
+        $cart += [$product => $quantity];
+    }            
+    else
+    {
+        echo "Invalid input<br/>";
+        $check = false;
+    }
+}
+
+function data($array)
+{
+    global $check;
+
+    foreach($array as $name => $count)
+        insert($name, $count);
+
+    result($check);
+}
+
+function insert($dbname, $dbcount)
+{
+    global $check;
+    $check = false;
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $databasename = "web_tech";
+    $tablename = "product_data";
+    $field1 = "product";
+    $field2 = "quantity";
+
+    $insert = "insert into $tablename ($field1, $field2) values ('$dbname', '$dbcount')";
+
+    $database = new mysqli($servername, $username, $password, $databasename);
+
+    if ($database->connect_error)    
+        die("Could not connect to database<br/>" . $database->conncet_error);
+
+    if($database->query($insert) === true)
+        $check = true;
+    else
+        die("Could not execute insertion!<br/>" . mysqli_error($database));
+
+    $database->close();
+}
+
+function result($success)
+{
+    if $success
+    {
+        print("Record inserted successfully!<br/>");
+        echo("Thank you!<br/>");
+    }
+
+    echo "<a href=\"index.html\">Previous Page</a>"; 
+}
+
+$cart = [];
+$check = true;
+
+addcart($_POST["productA"], $_POST["quantityA"]);
+addcart($_POST["productB"], $_POST["quantityB"]);
+addcart($_POST["productC"], $_POST["quantityC"]);
+
+if ($check)
+    data($cart);
+
+?>
+</body>
---
+++ Database/DatabaseV0.2/product_data.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-11-27 17:14:13
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `web_tech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `product_data`
+--
+
+CREATE TABLE `product_data` (
+  `id` int(11) NOT NULL,
+  `product` text NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `product_data`
+--
+ALTER TABLE `product_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `product_data`
+--
+ALTER TABLE `product_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
