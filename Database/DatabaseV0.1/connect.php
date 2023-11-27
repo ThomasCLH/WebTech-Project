beacdb4fd0d4bb4d24764c2289ae8cf5a8a3cf91
ThomasCLH <127458003+ThomasCLH@users.noreply.github.com>
2023-11-28 00:33:23 +0800

    connect.php

--- Database/DatabaseV0.1/connect.php
+++ Database/DatabaseV0.1/connect.php
@@ -5,31 +5,6 @@
 <body>
 <?php
 
-function insert($dbname, $dbcount)
-{
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $databasename = "web_tech";
-    $tablename = "product_data";
-    $field1 = "product";
-    $field2 = "quantity";
-
-    $insert = "insert into $table ($field1, $field2) values ('$dbname', '$dbcount')";
-
-    $database = new mysqli($servername, $username, $password, $databasename);
-
-    if ($database->connect_error)    
-        die("Could not connect to database" . $database->conncet_error);
-
-    if($database->query($insert) === true)
-        print("Record inserted successfully!<br/>");
-    else
-        die("Could not execute insertion!" . mysqli_error($database));
-
-    $database->close();
-}
-
 function data($name, $count)
 {
     if ($name != "" && $count > 0)
@@ -41,12 +16,42 @@ function data($name, $count)
         echo "Invalid input<br/>";
 }
 
+function insert($dbname, $dbcount)
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $databasename = "web_tech";
+    $tablename = "product_data";
+    $field1 = "product";
+    $field2 = "quantity";
+
+    $insert = "insert into $tablename ($field1, $field2) values ('$dbname', '$dbcount')";
+
+    $database = new mysqli($servername, $username, $password, $databasename);
+
+    if ($database->connect_error)    
+        die("Could not connect to database" . $sql->conncet_error);
+
+    if($database->query($insert) === true)
+        result();
+    else
+        die("Could not execute insertion!" . mysqli_error($database));
+
+    $database->close();
+}
+
+function result()
+{    
+    print("Record inserted successfully!<br/>");
+    echo("Thank you!<br/>");
+    echo "<a href=\"index.html\">Previous Page</a>"; 
+}
+
 $product = $_POST["product"];
 $quantity = $_POST["quantity"];
 
 data($product, $quantity);
-
-echo "<a href=\"index.html\">Previous Page</a>";
 
 ?>
 </body>
